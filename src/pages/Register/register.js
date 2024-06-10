aab07e7caf26daa054b8d89b7b367f41fe14918a
tiktokyi <tiktokyi@gmail.com>
2024-06-10 23:00:55 +0900

    Create : login 페이지 생성

---
+++ src/pages/Register/register.js
@@ -0,0 +1,13 @@
+import style from "./register.module.css";
+
+const Register = () => {
+    return (
+        <div className={style.container}>
+        <h1>
+            This is Register
+        </h1>
+        </div>
+    );
+};
+
+export default Register;
